82770e6866e7817760931a08a4345bfce49e9b5d
Kshitiz Pandya <110394695+KshitizPandya@users.noreply.github.com>
2023-03-25 14:28:17 +0530

    api key update

--- lib/Glot-AI/constants/api_consts.dart
+++ lib/Glot-AI/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1/chat";
-String API_KEY = "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";
+String API_KEY = "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";
